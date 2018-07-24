0d60f3fc52245093335166274f0d967496a2a79f
Manohar Negi <manohar@hurreh.com>
2018-07-24 16:45:09 +0530

    modified about page

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function (req, res, next) {
-  res.render('index', { layout: 'layout-index' });
+  res.render('index', { layout: 'layout-index', navHome: true });
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('about', { showFooter: true, navAbout: true });
+});
+
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { layout:'layout-contact', showFooter: true, navContact: true });
 });
 
 router.get('/resume', function(req, res, next) {
