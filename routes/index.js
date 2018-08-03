63aeb6429420095331b3517f11292ffd8c7ff4fa
Manohar Negi <manohar@hurreh.com>
2018-08-03 09:38:05 +0530

    added login logic

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,24 @@
 var express = require('express');
 var router = express.Router();
-var data = require('../mydata.json');
+var MongoClient = require('mongodb').MongoClient;
+// Connection URL
+var url = 'mongodb://localhost:27017';
+var db;
+// Use connect method to connect to the server
+MongoClient.connect(url, function(err, client) {
+  console.log("Connected successfully to MongoDB Server...");
+  db = client.db('portfolio');
+});
+
+function checkUser(userid, password, callback){
+  var collection = db.collection('users');
+  collection.find({'email': userid, 'password': password},{'password': 0}).toArray(function(err, docs) {
+    if(err)
+      callback(err, null);
+    else
+      callback(null, docs[0]);
+  });
+}
 
 router.get('/', function (req, res, next) {
   res.render('index', { layout: 'layout-index', navHome: true });
@@ -31,6 +49,26 @@ router.get('/signin', function(req, res, next) {
 });
 
 router.post('/signin', function(req, res, next) {
+  function doLogin(error, data){
+    if(error){
+      // handle it
+      var message = "Invalid email or password";
+      res.render('admin/signin', { layout: 'layout-signin', error: true, messages:[message] });
+    }
+
+    console.log(JSON.stringify(data));
+
+    req.session.isAuthenticated = true;
+    req.session.user = data;
+    res.locals.user = data;
+
+    res.render('admin/dashboard', { 
+      layout: 'layout-admin', 
+      title: 'Admin Dashboard',
+      navDashboard: true
+    });
+  };
+
   var email = req.body.email;
   var password = req.body.password;
   // validate inputs
@@ -39,7 +77,6 @@ router.post('/signin', function(req, res, next) {
       isEmail().withMessage('Please enter a valid email');
   req.checkBody('password', 'Password is required').notEmpty();
   var errors = req.validationErrors();
-  console.log('---- '+ JSON.stringify(errors));
   if (errors) {
     var messages = [];
     errors.forEach(function(error) {
@@ -48,22 +85,7 @@ router.post('/signin', function(req, res, next) {
     res.render('admin/signin', {layout:'layout-signin', error: messages.length > 0,messages: messages});
   }else{   
     // authenticate the user details
-    if(email && email !== '' && password && password !== '' &&
-        email === data.user.email && password === data.user.password){
-      // create the session
-      req.session.isAuthenticated = true;
-      req.session.user = {'email': email};
-      res.locals.user = {'email': email};
-
-      res.render('admin/dashboard', { 
-        layout: 'layout-admin', 
-        title: 'Admin Dashboard',
-        navDashboard: true
-      });
-    }else{
-      var message = "Invalid email or password";
-      res.render('admin/signin', { layout: 'layout-signin', error: true, messages:[message] });
-    }
+    checkUser(email, password, doLogin);
   }
 });
 
